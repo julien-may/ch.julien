544fdf7001a098578aa4ad5da5f3b6328c8493a2
Julien May <julien.may@gmail.com>
2013-06-24 19:34:44 +0200

    fixed treeset issue

--- query/src/main/java/ch/julien/query/core/DistinctIterator.java
+++ query/src/main/java/ch/julien/query/core/DistinctIterator.java
@@ -2,33 +2,31 @@ package ch.julien.query.core;
 
 import ch.julien.common.delegate.EqualityComparator;
 
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
-import java.util.TreeSet;
 
 class DistinctIterator<TSource> extends AbstractIterator<TSource, TSource> {
-	private final Set<Key<TSource>> set;
+	private final EqualityComparator<TSource> equalityComparator;
+	private final Set<Key<TSource>> set = new HashSet<Key<TSource>>();
 
 	public DistinctIterator(Iterator<? extends TSource> parent, EqualityComparator<TSource> equalityComparator) {
 		super(parent);
 
-		this.set = new TreeSet<Key<TSource>>(
-			new KeyComparator<TSource>(equalityComparator)
-		);
+		this.equalityComparator = equalityComparator;
 	}
 
 	@Override
 	protected TSource computeNext() {
 		while (parent.hasNext()) {
-			TSource item = parent.next();
-			Key<TSource> key = new Key<TSource>(item);
+			Key<TSource> key = new Key<TSource>(parent.next(), this.equalityComparator);
 
 			if (this.set.contains(key)) {
 				continue;
 			}
 
 			this.set.add(key);
-			return item;
+			return key.value;
 		}
 
 		return computationEnd();
